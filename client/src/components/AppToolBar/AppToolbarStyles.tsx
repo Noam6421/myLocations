5391110497896b26beb69ee901d2ea2653ee69bd
Noam6421 <noam6421@gmail.com>
2021-05-25 21:42:25 +0300

    intial structure and styles of appToolbar

--- client/src/components/AppToolBar/AppToolbarStyles.tsx
+++ client/src/components/AppToolBar/AppToolbarStyles.tsx
@@ -2,7 +2,20 @@ import { Theme } from '@material-ui/core';
 import { makeStyles } from '@material-ui/styles'
 
 const useStyles = makeStyles((theme: Theme) => ({
-
+    appBar: {
+        color: 'white',
+        backgroundColor: theme.palette.primary.light
+    },
+    title: {
+        color: 'white',
+        alignSelf: 'center'
+    },
+    menuItem: {
+        display: 'flex',
+        flexDirection: 'row',
+        justifyContent: 'space-between',
+        textDecoration: 'none',
+    },
 }));
 
 export default useStyles;
