2bed54b4a8d865afe36d86398e023ee7c0b152b3
Noam6421 <noam6421@gmail.com>
2021-05-26 23:00:26 +0300

    ui change when catgory selected

--- client/src/components/Content/Categories/CategoriesStyles.ts
+++ client/src/components/Content/Categories/CategoriesStyles.ts
@@ -12,9 +12,10 @@ const useStyles = makeStyles((theme: Theme) => ({
         margin: 'auto',
         transition: '0.3s',
         boxShadow: '0 8px 40px -12px rgba(0,0,0,0.3)',
-        '&:hover': {
-            boxShadow: '0 16px 70px -12.125px rgba(0,0,0,0.3)'
-        }
+    },
+    categoryCardSelected: {
+        boxShadow: '0 16px 70px -12.125px rgba(0,0,0,0.3)',
+        backgroundColor: theme.palette.primary.main,
     },
     categoryLink: {
         textDecoration: 'none',
