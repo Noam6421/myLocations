032f7fdc5d5a5e1bc27fc126cc80d914e9216fae
Noam6421 <noam6421@gmail.com>
2021-05-26 19:11:05 +0300

    changes in appToolbar

--- client/src/components/AppToolBar/AppToolbar.tsx
+++ client/src/components/AppToolBar/AppToolbar.tsx
@@ -1,31 +1,12 @@
   
 import React from 'react';
 import { AppBar, Grid, Toolbar, Typography } from '@material-ui/core';
-import { NavLink, NavLinkProps , useHistory } from 'react-router-dom';
+import { NavLink } from 'react-router-dom';
 
 import { indexRoute } from 'Utils/Routes/Routes';
 
 import useStyles from './AppToolbarStyles';
 import useAppToolbar from './useAppToolbar';
-
-const StatePersistentNavLink = (props: NavLinkProps) => {
-    const classes = useStyles();
-    const history = useHistory();
-
-    const handleNavClick: NavLinkProps['onClick'] = (event) => {
-        event.preventDefault(); // prevent state reset on reroute
-        history.push(props.to as string, history.location.state);
-    };
-
-    return (
-        <NavLink {...props} location={history.location}
-            onClick={handleNavClick}
-            className={classes.menuItem}
-        >
-            {props.children}
-        </NavLink>
-    );
-};
 
 const AppToolbar: React.FC = (): JSX.Element => {
 
@@ -35,10 +16,10 @@ const AppToolbar: React.FC = (): JSX.Element => {
         <AppBar className={classes.appBar} position='static'>
             <Toolbar>
                 <Grid container alignItems='center'>
-                    <StatePersistentNavLink exact to={indexRoute} >
+                    <NavLink exact to={indexRoute} className={classes.menuItem}>
                         <img id='logo' alt='logo' src='./assets/logo.png' width={79} height={60} />
                         <Typography variant='h5' className={classes.title} id='title'>myLocations</Typography>
-                    </StatePersistentNavLink>
+                    </NavLink>
                 </Grid>
             </Toolbar>
         </AppBar>
