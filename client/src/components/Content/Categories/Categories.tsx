e4b3cbdfa4129ac2a7c387cf3bc51a28f5b05a8e
Noam6421 <noam6421@gmail.com>
2021-05-26 19:12:10 +0300

    intial look of categoriesPage

--- client/src/components/Content/Categories/Categories.tsx
+++ client/src/components/Content/Categories/Categories.tsx
@@ -1,16 +1,47 @@
 import React from 'react';
 import { useSelector } from 'react-redux';
+import { Grid, Typography, Card } from '@material-ui/core';
 
 import Category from 'models/Category';
 import StoreStateType from 'redux/storeStateType';
 
+import useStyles from './CategoriesStyles';
+import { Link } from 'react-router-dom';
+import { indexRoute } from 'Utils/Routes/Routes';
+
 const Categories: React.FC<Props> = (): JSX.Element => {
+    
+    const classes = useStyles();
 
     const categories = useSelector<StoreStateType, Category[]>(state => state.categories)
 
     console.log(categories);
     return (
-        <p>Categories</p>
+        <>
+            <Grid 
+                container 
+                spacing={2} 
+                direction='row' 
+                justify='flex-start' 
+                alignItems='flex-start' 
+                className={classes.categoriesSection}
+            >
+                <Grid item xs={12}>
+                    <Typography>Categories:</Typography>
+                </Grid>
+                {
+                    categories.map((category) => {
+                        return (
+                            <Grid item xs={3} key={category.name}>
+                                <Link to={indexRoute} className={classes.categoryLink}>
+                                    <Card className={classes.categoryCard}>{category.name}</Card>
+                                </Link>
+                            </Grid>
+                        )
+                    })
+                }
+            </Grid>
+        </>
     )
 }
 
---
+++ client/src/components/Content/Categories/CategoriesStyles.ts
@@ -0,0 +1,24 @@
+import { Theme } from '@material-ui/core';
+import { makeStyles } from '@material-ui/styles'
+
+const useStyles = makeStyles((theme: Theme) => ({
+    categoriesSection: {
+        padding: '20px 40px',
+    },
+    categoryCard: {
+        color: 'white',
+        backgroundColor: theme.palette.primary.light,
+        maxWidth: 300,
+        margin: 'auto',
+        transition: '0.3s',
+        boxShadow: '0 8px 40px -12px rgba(0,0,0,0.3)',
+        '&:hover': {
+            boxShadow: '0 16px 70px -12.125px rgba(0,0,0,0.3)'
+        }
+    },
+    categoryLink: {
+        textDecoration: 'none',
+    },
+}));
+
+export default useStyles;
