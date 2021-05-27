3e409fa8c11146b6ce1b3a2950f7524a1ef67124
Noam6421 <noam6421@gmail.com>
2021-05-27 10:14:25 +0300

    integrated local storage to redux

--- client/src/redux/store.ts
+++ client/src/redux/store.ts
@@ -3,16 +3,41 @@ import { createStore, applyMiddleware, compose } from 'redux';
 import storageSession from 'redux-persist/lib/storage/session';
 import { persistStore, persistReducer, PersistConfig } from 'redux-persist'
 
+import Category from 'models/Category';
+
 import reducers from './reducers';
 import StoreStateType from './storeStateType';
 
+const saveToLocalStorage = (categories: Category[]) => {
+    try {
+        const localStorageCategories = JSON.stringify(categories);
+        localStorage.setItem('categories', localStorageCategories);
+    } catch (e) {
+        console.warn(e);
+    }
+};
+
+const loadFromLocalStorage = () => {
+    try {
+        const localStorageCategories = localStorage.getItem('categories');
+        if (localStorageCategories === null) return undefined;
+        return JSON.parse(localStorageCategories);
+    } catch (e) {
+        console.warn(e);
+        return undefined;
+    }
+};
+  
 const persistConfig: PersistConfig<StoreStateType> = {
     key: 'root',
     storage: storageSession
 };
 
-const persistedReducer = persistReducer(persistConfig, reducers)
+const persistedReducer = persistReducer(persistConfig, reducers);
 const composeEnhancers = (window as any).__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 
-export const store = createStore(persistedReducer, composeEnhancers(applyMiddleware(thunk)));
+export const store = createStore(persistedReducer, loadFromLocalStorage(), composeEnhancers(applyMiddleware(thunk)));
+
 export const persistor = persistStore(store);
+
+store.subscribe(() => saveToLocalStorage(store.getState().categories));
