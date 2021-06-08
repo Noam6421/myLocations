d757f8429c3306776214aee28e5767bf60bd0eda
Noam6421 <noam6421@gmail.com>
2021-06-08 09:56:38 +0300

    new ViewOptions Type and inital state

---
+++ client/src/models/ViewOptions.ts
@@ -0,0 +1,9 @@
+interface ViewOptions {
+    sort: string;
+    groupBy: string;
+    filter: string
+};
+
+export const initalViewOptions = {sort: '', groupBy: '', filter: ''};
+
+export default ViewOptions;
